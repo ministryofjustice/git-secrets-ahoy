b947598e3fd650000e1095468554dfbc95e6f696
Glen Mailer <glen@stainlessed.co.uk>
2018-03-03 19:09:43 +0000

    high-level process sketch

--- git_secrets_ahoy/__init__.py
+++ git_secrets_ahoy/__init__.py
@@ -1,5 +1,44 @@
+from collections import namedtuple
+
+Context = namedtuple('Context', ['git_repo'])
+Secret = namedtuple('Secret', ['commit', 'type'])
+
 def main():
-    print("hello")
+    context = parse_args()
+    secrets = scan_repo()
+    output = format_output(secrets)
+    print(output)
+    return 0 if not secrets else 1
+
+def parse_args():
+    pass
+
+def scan_repo():
+    repo = load_repo()
+    commits = find_relevant_commits(repo)
+    return find_secrets(commits)
+
+def load_repo():
+    pass
+
+def find_relevant_commits(repo):
+    return []
+
+def find_secrets(commits):
+    secrets = []
+    for commit in commits:
+        matches = check_commit(commit)
+        if matches:
+            secrets.extend(matches)
+    return secrets
+
+def check_commit():
+    pass
+
+def format_output(secrets):
+    return "Stuff and Things"
 
 if __name__ == '__main__':
-    main()
+    import sys
+    code = main()
+    sys.exit(code)
